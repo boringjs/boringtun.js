631f8b79a268e50b9758476ead80c346b54f741f
Andrei Smolnikov <andr.sml@gmail.com>
2024-04-01 19:37:52 +0300

    Multi platform fix

--- src/tunnel.js
+++ src/tunnel.js
@@ -1,6 +1,6 @@
 const PLATFORMS = {
-  ["darwin@arm64"]: '../build/mac/boringtunjs.node',
-  ["darwin@intel"]: '../build/mac/boringtunjs.node'
+  ["darwin@arm64"]: '../build/mac_m/boringtunjs.node',
+  ["darwin@intel"]: '../build/mac_intel/boringtunjs.node'
 }
 
 const pathToBindings = PLATFORMS[`${process.platform}@${process.arch}`]
