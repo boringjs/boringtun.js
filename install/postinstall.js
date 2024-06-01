3526ef75af2fc3e936a7dfb487040148f4966663
Andrei Smolnikov <andr.sml@gmail.com>
2024-06-01 19:21:51 +0300

    lint: postinstall.js

--- install/postinstall.js
+++ install/postinstall.js
@@ -14,20 +14,14 @@ const buildDir = path.join(__dirname, '..', 'build', 'lib')
 const bindingsPath = path.join(buildDir, 'boringtunjs.node')
 
 if (!fs.existsSync(bindingsPath)) {
-  fs.mkdirSync(buildDir, {recursive: true})
-  fetch(downloadURL).then(res => {
-    if (res.status !== 200) {
-      console.warn('Boringtunjs: not supported platform. Please report issue.')
-    }
-    return res.arrayBuffer()
-  })
-    .then(file => fs.writeFileSync(bindingsPath, Buffer.from(file)))
+  fs.mkdirSync(buildDir, { recursive: true })
+  fetch(downloadURL)
+    .then((res) => {
+      if (res.status !== 200) {
+        console.warn('Boringtunjs: not supported platform. Please report issue.')
+      }
+      return res.arrayBuffer()
+    })
+    .then((file) => fs.writeFileSync(bindingsPath, Buffer.from(file)))
     .catch(console.error)
 }
-
-
-
-
-
-
-
