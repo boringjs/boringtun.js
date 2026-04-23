3d46bc3d81b824e64ebdcf48d9eb393c92a79bab
Andrei Smolnikov <andr.sml@gmail.com>
2026-04-23 14:06:05 +0300

    Remove jest config

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,4 @@
 const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended')
-const jest = require('eslint-plugin-jest')
 const js = require('@eslint/js')
 const globals = require('globals')
 
@@ -8,7 +7,6 @@ module.exports = [
     languageOptions: {
       globals: {
         ...globals.node,
-        ...globals.jest,
       },
     },
   },
@@ -17,8 +15,4 @@ module.exports = [
     files: ['**/**/*.js'],
   },
   eslintPluginPrettierRecommended,
-  {
-    ...jest.configs['flat/recommended'],
-    files: ['./tests/**/*.(test|spec).js'],
-  },
 ]
