e795b65557407b323aeca9134b65456535a77263
Andrei Smolnikov <andr.sml@gmail.com>
2026-04-28 13:47:42 +0300

    Forward peerId when creating UDP clients
    #createUDP destructures peerId and passes it to UDPClient/DNSResolver,
    but the call site omitted it, so downstream udpSocketFactory always
    saw peerId: undefined even when the IP4 packet carried one.

--- src/protocols/udp-container.js
+++ src/protocols/udp-container.js
@@ -106,6 +106,7 @@ class UdpContainer extends EventEmitter {
 
     if (!this.#udpClients.has(hash)) {
       this.#createUDP({
+        peerId,
         sourceIP,
         destinationIP,
         sourcePort,
